7b2237064ed2ab4375ad170a1485943bf93eef4c
Khaled Nassar <knassar702@gmail.com>
2024-10-26 23:47:02 +0300

    Debug mode

--- run.py
+++ run.py
@@ -157,7 +157,8 @@ class SonarScanner:
             f"-Dsonar.login={self.sonar_token}",
             f"-Dsonar.externalIssuesReportPaths={sarif_files_str}",  # Use externalIssuesReportPaths for multiple files
             f"-Dsonar.exclusions={self.exclude}",
-            f"-Dsonar.sources=."
+            f"-Dsonar.sources=.",
+            "-Dsonar.verbose=true"
         ]
 
         print(f"Running sonar-scanner with reports: {sarif_files_str}")
