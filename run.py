76af42e7db9fbe8fdaa19b43efeb9927c934902b
Khaled Nassar <knassar702@gmail.com>
2024-10-27 06:31:42 +0300

    more disables

--- run.py
+++ run.py
@@ -118,7 +118,7 @@ class SonarScanner:
             f"-Dsonar.token={self.sonar_token}",
             f"-Dsonar.externalIssuesReportPaths={sarif_files_str}",  # Use externalIssuesReportPaths for multiple files
             f"-Dsonar.sources=.",
-            f"-Dsonar.exclusions=*.jar,**/*.jar,gradle/**"
+            f"-Dsonar.exclusions=**/*.jar"
             #"-Dsonar.verbose=true",
             #"-Dsonar.issue.ignore.multicriteria.e1.resourceKey=*"
             #"-Dsonar.language="
